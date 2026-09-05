4900ad9174fbbb723e8cf53e8e41d4bdc3d78ad0
Hinn06 <chiennhuxk09@gmail.com>
2026-09-05 16:20:12 +0700

    Delete BTL_web directory

--- BTL_web/src/Core/Controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace App\Core;
-abstract class Controller {
-    protected function view(string $name, array $data=[]): void { extract($data); require dirname(__DIR__) . '/View/' . $name . '.php'; }
-}
